5dc27692df2e7ec9cd9e6d5a78c769fee3730c69
xiomararomero98 <xio.romero@duocuc.cl>
2026-05-12 19:29:11 -0400

    microservicio para gestionar proyectos

---
+++ ms-proyectos/ms-proyectos/src/main/java/com/innovacore/ms_proyectos/MsProyectosApplication.java
@@ -0,0 +1,13 @@
+package com.innovacore.ms_proyectos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProyectosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsProyectosApplication.class, args);
+	}
+
+}
---
+++ ms-proyectos/ms-proyectos/src/test/java/com/innovacore/ms_proyectos/MsProyectosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innovacore.ms_proyectos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProyectosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
