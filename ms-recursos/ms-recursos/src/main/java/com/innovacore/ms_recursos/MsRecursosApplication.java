5a0056b829287682cbc743fe2288d39c1e42e6bf
xiomararomero98 <xio.romero@duocuc.cl>
2026-05-12 20:34:20 -0400

    Microservicio recursos

---
+++ ms-recursos/ms-recursos/src/main/java/com/innovacore/ms_recursos/MsRecursosApplication.java
@@ -0,0 +1,13 @@
+package com.innovacore.ms_recursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRecursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsRecursosApplication.class, args);
+	}
+
+}
---
+++ ms-recursos/ms-recursos/src/test/java/com/innovacore/ms_recursos/MsRecursosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innovacore.ms_recursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRecursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
